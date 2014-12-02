47c3c74e7e29210466b61c10bf9098f415e4e52e
kranitz <kranitzgabor@vipmail.hu>
2014-12-02 01:04:19 +0100

    - gombnyomasra fajlbolt betoltheto egy palya - celvonal kezdo/vegpontjat bekattintva megrajzolja

--- WebCam/WebCam/Form1.Designer.cs
+++ WebCam/WebCam/Form1.Designer.cs
@@ -25,12 +25,15 @@
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.open_image_btn = new System.Windows.Forms.Button();
             this.Save = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.set_finish_line_btn = new System.Windows.Forms.Button();
+            this.open_img_btn = new System.Windows.Forms.Button();
+            this.capture_new_track_btn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,22 +46,22 @@
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
-            // button1
+            // open_image_btn
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(287, 411);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.open_image_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.open_image_btn.Location = new System.Drawing.Point(134, 428);
+            this.open_image_btn.Name = "open_image_btn";
+            this.open_image_btn.Size = new System.Drawing.Size(75, 23);
+            this.open_image_btn.TabIndex = 3;
+            this.open_image_btn.Text = "Open Image";
+            this.open_image_btn.UseVisualStyleBackColor = true;
+            this.open_image_btn.Visible = false;
+            this.open_image_btn.Click += new System.EventHandler(this.button1_Click);
             // 
             // Save
             // 
             this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.Save.Location = new System.Drawing.Point(382, 411);
+            this.Save.Location = new System.Drawing.Point(620, 411);
             this.Save.Name = "Save";
             this.Save.Size = new System.Drawing.Size(75, 23);
             this.Save.TabIndex = 4;
@@ -71,7 +74,7 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(438, 120);
+            this.label1.Location = new System.Drawing.Point(676, 120);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(17, 13);
             this.label1.TabIndex = 6;
@@ -82,7 +85,7 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(461, 120);
+            this.label2.Location = new System.Drawing.Point(699, 120);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 8;
@@ -93,7 +96,7 @@
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(438, 145);
+            this.label3.Location = new System.Drawing.Point(676, 145);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(17, 13);
             this.label3.TabIndex = 10;
@@ -104,23 +107,60 @@
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(461, 145);
+            this.label4.Location = new System.Drawing.Point(699, 145);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "label4";
             // 
+            // set_finish_line_btn
+            // 
+            this.set_finish_line_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.set_finish_line_btn.Location = new System.Drawing.Point(620, 12);
+            this.set_finish_line_btn.Name = "set_finish_line_btn";
+            this.set_finish_line_btn.Size = new System.Drawing.Size(114, 23);
+            this.set_finish_line_btn.TabIndex = 13;
+            this.set_finish_line_btn.Text = "Set Finish Line";
+            this.set_finish_line_btn.UseVisualStyleBackColor = true;
+            this.set_finish_line_btn.Click += new System.EventHandler(this.set_finish_line_btn_Click);
+            // 
+            // open_img_btn
+            // 
+            this.open_img_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.open_img_btn.Location = new System.Drawing.Point(527, 411);
+            this.open_img_btn.Name = "open_img_btn";
+            this.open_img_btn.Size = new System.Drawing.Size(75, 23);
+            this.open_img_btn.TabIndex = 14;
+            this.open_img_btn.Text = "Open Image";
+            this.open_img_btn.UseVisualStyleBackColor = true;
+            this.open_img_btn.Click += new System.EventHandler(this.open_img_btn_Click);
+            // 
+            // capture_new_track_btn
+            // 
+            this.capture_new_track_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.capture_new_track_btn.Location = new System.Drawing.Point(374, 411);
+            this.capture_new_track_btn.Name = "capture_new_track_btn";
+            this.capture_new_track_btn.Size = new System.Drawing.Size(114, 23);
+            this.capture_new_track_btn.TabIndex = 15;
+            this.capture_new_track_btn.Text = "Capture New Track";
+            this.capture_new_track_btn.UseVisualStyleBackColor = true;
+            this.capture_new_track_btn.Visible = false;
+            this.capture_new_track_btn.Click += new System.EventHandler(this.capture_new_track_btn_Click);
+            // 
             // mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(508, 453);
+            this.ClientSize = new System.Drawing.Size(746, 453);
+            this.Controls.Add(this.capture_new_track_btn);
+            this.Controls.Add(this.open_img_btn);
+            this.Controls.Add(this.set_finish_line_btn);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Save);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.open_image_btn);
             this.Controls.Add(this.imageBox1);
             this.Name = "mainform";
             this.Text = "WebCam";
@@ -134,12 +174,15 @@
         #endregion
 
         private Emgu.CV.UI.ImageBox imageBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button open_image_btn;
         private System.Windows.Forms.Button Save;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button set_finish_line_btn;
+        private System.Windows.Forms.Button open_img_btn;
+        private System.Windows.Forms.Button capture_new_track_btn;
     }
 }
 
--- WebCam/WebCam/Form1.cs
+++ WebCam/WebCam/Form1.cs
@@ -13,160 +13,249 @@ using Emgu.CV.Structure;
 using Emgu.Util;
 
 namespace WebCam {
-	public partial class mainform : Form {
+    public partial class mainform : Form {
 
-		public mainform() {
-			InitializeComponent();
-			button1.Text = "Start!";
-			Save.Enabled = Enabled;
+        public mainform() {
+            InitializeComponent();
+            open_image_btn.Text = "Start!";
+            Save.Enabled = Enabled;
+            set_finish_line_btn.Enabled = false;
 
-		}
-
+        }
+        Game_Controller gc = new Game_Controller();
         int pic_widht = 800;
         int pic_height = 600;
 
-		private void mainform_Load(object sender, EventArgs e) {
+        private void mainform_Load(object sender, EventArgs e) {
 
-			capture = new Capture();
+            NewCapture();
+        }
 
-			this.Width = pic_widht+100;
-			this.Height = pic_height+100;
-			label2.Text = "-";
-			label4.Text = "-";
-            
-			capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS, 40);
-			capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, pic_widht);
-			capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, pic_height);
+        private void NewCapture() {
+            capture = new Capture();
 
-			Application.Idle += ProcessFrame;
+            this.Width = pic_widht + 100;
+            this.Height = pic_height + 100;
+            label2.Text = "-";
+            label4.Text = "-";
 
-		}
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS, 40);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, pic_widht);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, pic_height);
 
-		private Capture capture;        //takes images from camera as image frames
-		private bool captureInProgress; // checks if capture is executing
+            Application.Idle += ProcessFrame;
+        }
 
-		private void ProcessFrame(object sender, EventArgs arg) {
-			Image<Bgr, Byte> ImageFrame = capture.QueryFrame();  //line 1
-			imageBox1.Image = ImageFrame;        //line 2
-		}
+        private Capture capture;        //takes images from camera as image frames
+        private bool captureInProgress; // checks if capture is executing
 
-		private void button1_Click(object sender, EventArgs e) {
+        private void ProcessFrame(object sender, EventArgs arg) {
+            Image<Bgr, Byte> ImageFrame = capture.QueryFrame();  //line 1
+            imageBox1.Image = ImageFrame;        //line 2
+        }
 
-			#region if capture is not created, create it now
-			if(capture == null) {
-				try {
-					capture = new Capture();
-				} catch(NullReferenceException excpt) {
-					MessageBox.Show(excpt.Message);
-				}
-			}
-			#endregion
+        private void button1_Click(object sender, EventArgs e) {
 
-
-			if(capture != null) {
-				if(captureInProgress) {  //if camera is getting frames then stop the capture and set button Text
-					// "Start" for resuming capture
-					button1.Text = "Start!"; //
-					Application.Idle -= ProcessFrame;
-
-				} else {
-					//if camera is NOT getting frames then start the capture and set button
-					// Text to "Stop" for pausing capture
-					button1.Text = "Stop";
-					Application.Idle += ProcessFrame;
-					Save.Enabled = true;
-				}
-
-				captureInProgress = !captureInProgress;
-			}
-		}
-
-		private void ReleaseData() {
-			if(capture != null)
-				capture.Dispose();
-		}
-
-		private void Save_Click(object sender, EventArgs e) {
-			SaveFileDialog save_file_dialog = new SaveFileDialog();
-			//save_file_dialog.ShowDialog();
-			save_file_dialog.RestoreDirectory = true;
-			save_file_dialog.Filter = "JPG fájl | *.jpg";
-			Stream myStream;
+            #region if capture is not created, create it now
+            if (capture == null) {
+                try {
+                    capture = new Capture();
+                } catch (NullReferenceException excpt) {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
 
 
-			if(save_file_dialog.ShowDialog() == DialogResult.OK) {
-				imageBox1.Image.Save(save_file_dialog.FileName);
-			}
-			//Image img = Image.FromFile();
-			Bitmap bm = new Bitmap(save_file_dialog.FileName);
-			reload_pic_and_draw_intersection(bm);
-		}
+            if (capture != null) {
+                if (captureInProgress) {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    open_image_btn.Text = "Start!"; //
+                    Application.Idle -= ProcessFrame;
 
-		private void reload_pic_and_draw_intersection(Bitmap img) {
-			PictureBox pb = new PictureBox();
-			pb.Image = img;
+                } else {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    open_image_btn.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                    Save.Enabled = true;
+                }
 
-			Color c = new Color();
-			c = Color.White;
-			for(int i = 0; i < imageBox1.Image.Bitmap.Width - 1; i+=25) {
-				for(int j = 0; j < imageBox1.Image.Bitmap.Height - 1; j+=25) {
-					if((i % 25 == 0) && (j % 25 == 0)) {
-						img.SetPixel(i, j, c);
-						intersections[i, j] = 1;
-						if(i > 0 && j > 0 && i < pic_widht && j < pic_height) {
-							img.SetPixel(i, j, c);
-							img.SetPixel(i + 1, j, c);
-							img.SetPixel(i - 1, j, c);
-							img.SetPixel(i, j + 1, c);
-							img.SetPixel(i, j - 1, c);
-							img.SetPixel(i + 1, j + 1, c);
-							img.SetPixel(i - 1, j - 1, c);
-							img.SetPixel(i + 1, j - 1, c);
-							img.SetPixel(i - 1, j + 1, c);
+                captureInProgress = !captureInProgress;
+            }
+        }
 
-							intersections[i, j] = 1;
-							intersections[i + 1, j] = 1;
-							intersections[i - 1, j] = 1;
-							intersections[i, j + 1] = 1;
-							intersections[i, j - 1] = 1;
-							intersections[i + 1, j + 1] = 1;
-							intersections[i + 1, j - 1] = 1;
-							intersections[i - 1, j + 1] = 1;
-							intersections[i - 1, j - 1] = 1;
+        private void ReleaseData() {
+            if (capture != null)
+                capture.Dispose();
+        }
+        Bitmap track;
+        private void Save_Click(object sender, EventArgs e) {
+            SaveFileDialog save_file_dialog = new SaveFileDialog();
+            //save_file_dialog.ShowDialog();
+            save_file_dialog.RestoreDirectory = true;
+            save_file_dialog.Filter = "JPG fájl | *.jpg";
+            Stream myStream;
 
-						}
-					}
-				}
-			}
 
-			pb.Width = img.Width;
-			pb.Height = img.Height;
-			this.Controls.Add(pb);
-			pb.MouseClick += check_where_lmb_clicked;
-			imageBox1.Visible = false;
+            if (save_file_dialog.ShowDialog() == DialogResult.OK) {
+                imageBox1.Image.Save(save_file_dialog.FileName);
+            }
+            //Image img = Image.FromFile();
+            track = new Bitmap(save_file_dialog.FileName);
+            reload_pic_and_draw_intersection(track);
+        }
+        PictureBox pb = new PictureBox();
+        private void reload_pic_and_draw_intersection(Bitmap img) {
+            set_finish_line_btn.Enabled = true;
 
-		}
+            pb.Image = img;
 
-		private void check_where_lmb_clicked(object sender, MouseEventArgs e) {
-			if(e.Button == MouseButtons.Left) {
-				if(intersections[e.X, e.Y] == 1) {
-					set_labels_visible(true);
-					label2.Text = e.X.ToString();
-					label4.Text = e.Y.ToString();
-				} else {
-					// set_labels_visible(false);
-				}
-			}
-		}
+            Color c = new Color();
+            c = Color.White;
+            for (int i = 0; i < imageBox1.Image.Bitmap.Width - 1; i += 25) {
+                for (int j = 0; j < imageBox1.Image.Bitmap.Height - 1; j += 25) {
+                    if ((i % 25 == 0) && (j % 25 == 0)) {
+                        img.SetPixel(i, j, c);
+                        intersections[i, j] = 1;
+                        if (i > 0 && j > 0 && i < pic_widht && j < pic_height) {
+                            img.SetPixel(i, j, c);
+                            img.SetPixel(i + 1, j, c);
+                            img.SetPixel(i - 1, j, c);
+                            img.SetPixel(i, j + 1, c);
+                            img.SetPixel(i, j - 1, c);
+                            img.SetPixel(i + 1, j + 1, c);
+                            img.SetPixel(i - 1, j - 1, c);
+                            img.SetPixel(i + 1, j - 1, c);
+                            img.SetPixel(i - 1, j + 1, c);
 
-		private void set_labels_visible(bool state) {
-			label1.Visible = state;
-			label2.Visible = state;
-			label3.Visible = state;
-			label4.Visible = state;
-		}
+                            intersections[i, j] = 1;
+                            intersections[i + 1, j] = 1;
+                            intersections[i - 1, j] = 1;
+                            intersections[i, j + 1] = 1;
+                            intersections[i, j - 1] = 1;
+                            intersections[i + 1, j + 1] = 1;
+                            intersections[i + 1, j - 1] = 1;
+                            intersections[i - 1, j + 1] = 1;
+                            intersections[i - 1, j - 1] = 1;
 
-		int[,] intersections = new int[800, 600];
+                        }
+                    }
+                }
+            }
 
-	}
+            pb.Width = img.Width;
+            pb.Height = img.Height;
+            pb.Invalidate();
+            pb.Update();
+            this.Controls.Add(pb);
+            imageBox1.Visible = false;
+            fl_counter = 0;
+        }
+        //Bitmap img, Point A, Point B
+        public void DrawFinishLine(Bitmap img, Point A, Point B) {
+            Pen pen = new Pen(Color.Red, 3);
+            // Draw line to screen.
+            using (var graphics = Graphics.FromImage(img)) {
+                graphics.DrawLine(pen, A, B);
+
+            }
+        }
+
+        public void check_where_lmb_clicked(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                if (intersections[e.X, e.Y] == 1) {
+                    set_labels_visible(true);
+                    label2.Text = e.X.ToString();
+                    label4.Text = e.Y.ToString();
+                } else {
+                    // set_labels_visible(false);
+                }
+            }
+            Point p = new Point(e.X, e.Y);
+
+            switch (fl_counter) {
+                case 0:
+                    gc.finish_line_start = p;
+
+                    break;
+                case 1:
+                    gc.finish_line_end = p;
+                    break;
+                default:
+                    break;
+            }
+            if (fl_counter == 1) {
+                DrawFinishLine(track, gc.finish_line_start, gc.finish_line_end);
+            }
+            fl_counter++;
+            pb.Invalidate();
+            pb.Update();
+        }
+
+        private void set_labels_visible(bool state) {
+            label1.Visible = state;
+            label2.Visible = state;
+            label3.Visible = state;
+            label4.Visible = state;
+        }
+
+        int[,] intersections = new int[800, 600];
+
+
+
+        int fl_counter = 0;
+        private void set_finish_line_btn_Click(object sender, EventArgs e) {
+            List<Point> p = new List<Point>();
+            pb.MouseClick += check_where_lmb_clicked;
+            fl_counter = 0;
+            //imageBox1.MouseClick += check_where_lmb_clicked;
+
+        }
+
+        private void open_img_btn_Click(object sender, EventArgs e) {
+            pb.MouseClick -= check_where_lmb_clicked;
+            fl_counter = 0;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            //DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            openFileDialog1.Title = "Open an Image file";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                track = new Bitmap(openFileDialog1.FileName);
+                reload_pic_and_draw_intersection(track);
+
+                
+            }
+        }
+
+        private void capture_new_track_btn_Click(object sender, EventArgs e) {
+            NewCapture();
+        }
+
+    }
+}
+
+class Game_Controller {
+
+    private Point fl_start;
+    private Point fl_end;
+    private List<Point[]> fl_points;
+
+    public Point finish_line_start {
+        get { return fl_start; }
+        set { fl_start = value; }
+    }
+    public Point finish_line_end {
+        get { return fl_end; }
+        set { fl_end = value; }
+    }
+}
+
+class Player {
+    private Point position;
+    private Color color;
+    public Point Position {
+        get { return position; }
+        set { position = value; }
+    }
 }
